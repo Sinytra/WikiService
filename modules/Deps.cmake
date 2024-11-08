a959c65017a856fa956557963caee5cbf25330dc
Su5eD <su5ed@outlook.com>
2024-11-08 12:12:39 +0100

    Attempt to get it working on Windows

--- modules/Deps.cmake
+++ modules/Deps.cmake
@@ -1,22 +1,56 @@
 include(modules/CPM.cmake)
 
-#CPMAddPackage(
-#        NAME SPDLOG
-#        GITHUB_REPOSITORY gabime/spdlog
-#        GIT_TAG v1.11.0
-#        OPTIONS
-#        "SPDLOG_BUILD_SHARED OFF"
-#        "SPDLOG_BUILD_EXAMPLE OFF"
-#        "SPDLOG_BUILD_EXAMPLE_HO OFF"
-#        "SPDLOG_BUILD_TESTS OFF"
-#        "SPDLOG_BUILD_TESTS_HO OFF"
-#        "SPDLOG_BUILD_BENCH OFF"
-#        "SPDLOG_SANITIZE_ADDRESS OFF"
-#        "SPDLOG_INSTALL OFF"
-#        "SPDLOG_FMT_EXTERNAL OFF"
-#        "SPDLOG_FMT_EXTERNAL_HO OFF"
-#        "SPDLOG_NO_EXCEPTIONS OFF"
-#)
+IF (WIN32 AND USE_LOCAL_DEPS)
+    CPMAddPackage(
+            NAME SPDLOG
+            GITHUB_REPOSITORY gabime/spdlog
+            GIT_TAG v1.11.0
+    )
+
+    CPMAddPackage(
+            NAME Hiredis
+            GITHUB_REPOSITORY redis/hiredis
+            GIT_TAG v1.2.0
+            OPTIONS
+            "BUILD_SHARED_LIBS OFF"
+    )
+    if (Hiredis_ADDED)
+        add_library(Hiredis_lib ALIAS hiredis)
+
+        file(MAKE_DIRECTORY ${hiredis_SOURCE_DIR}/include/hiredis)
+        file(GLOB HIREDIS_HEADERS "${hiredis_SOURCE_DIR}/*.h")
+        foreach(header ${HIREDIS_HEADERS})
+            configure_file(${header} ${hiredis_SOURCE_DIR}/include/hiredis COPYONLY)
+        endforeach()
+
+        target_include_directories(hiredis PUBLIC
+                $<BUILD_INTERFACE:${hiredis_SOURCE_DIR}/include>
+                $<INSTALL_INTERFACE:include>
+        )
+    endif()
+
+    CPMAddPackage(
+            NAME jsoncpp
+            GITHUB_REPOSITORY open-source-parsers/jsoncpp
+            GIT_TAG 1.9.6
+    )
+    if (jsoncpp_ADDED)
+        add_library(Jsoncpp_lib ALIAS jsoncpp_static)
+    endif()
+
+    CPMAddPackage(
+            NAME drogon
+            GITHUB_REPOSITORY drogonframework/drogon
+            GIT_TAG v1.9.8
+            OPTIONS
+            "BUILD_EXAMPLES OFF"
+            "BUILD_SHARED_LIBS OFF"
+            "BUILD_REDIS ON"
+    )
+    if (drogon_ADDED)
+        add_library(Drogon::Drogon ALIAS drogon)
+    endif()
+ENDIF()
 
 CPMAddPackage(
         NAME jwt-cpp
