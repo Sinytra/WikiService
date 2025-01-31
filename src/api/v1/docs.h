3d85e2785ae4fd3b3cb60b59515a35493c2eb0ad
Su5eD <su5ed@outlook.com>
2025-01-31 21:00:57 +0100

    Cleanup route paths to avoid conflicts

--- src/api/v1/docs.h
+++ src/api/v1/docs.h
@@ -11,10 +11,11 @@ namespace api::v1 {
         explicit DocsController(Database &, Storage &);
 
         METHOD_LIST_BEGIN
-        ADD_METHOD_TO(DocsController::project, "/api/v1/docs/{1:project}", drogon::Get, "AuthFilter");
-        ADD_METHOD_TO(DocsController::page, "/api/v1/project/{1:project}/page/.*", drogon::Get, "AuthFilter");
-        ADD_METHOD_TO(DocsController::tree, "/api/v1/project/{1:project}/tree", drogon::Get, "AuthFilter");
-        ADD_METHOD_TO(DocsController::asset, "/api/v1/project/{1:project}/asset/.*", drogon::Get); // Public
+        ADD_METHOD_TO(DocsController::project,  "/api/v1/docs/{1:project}",           drogon::Get, "AuthFilter");
+        ADD_METHOD_TO(DocsController::page,     "/api/v1/docs/{1:project}/page/.*",   drogon::Get, "AuthFilter");
+        ADD_METHOD_TO(DocsController::tree,     "/api/v1/docs/{1:project}/tree",      drogon::Get, "AuthFilter");
+        // Public
+        ADD_METHOD_TO(DocsController::asset,    "/api/v1/docs/{1:project}/asset/.*",  drogon::Get);
         METHOD_LIST_END
 
         drogon::Task<> project(drogon::HttpRequestPtr req, std::function<void(const drogon::HttpResponsePtr &)> callback,
